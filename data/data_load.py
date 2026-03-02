2af21791c6cb5fd4335400f7e49821e41d256e2f
OlehRoman <oleh.roman.shi.2023@lpnu.ua>
2026-03-02 16:31:20 +0200

    fix data_load

--- data/data_load.py
+++ data/data_load.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 import kagglehub
+import sys
 
-BASE_DIR = Path(__file__).resolve().parent
-RAW_DIR = BASE_DIR / "raw"
-RAW_DIR.mkdir(parents=True, exist_ok=True)
+try:
+    BASE_DIR = Path(__file__).resolve().parent
+    RAW_DIR = BASE_DIR / "raw"
+    RAW_DIR.mkdir(parents=True, exist_ok=True)
 
-path = kagglehub.dataset_download(
-    "elemento/nyc-yellow-taxi-trip-data",
-    path=str(RAW_DIR)
-)
+    print("Починаю завантаження датасету...")
+    path = kagglehub.dataset_download(
+        "elemento/nyc-yellow-taxi-trip-data",
+        path=str(RAW_DIR)
+    )
+    print("Downloaded to:", path)
 
-print("Downloaded to:", path)
+except Exception as e:
+    print(f"Помилка при завантаженні: {e}")
+    sys.exit(1)
